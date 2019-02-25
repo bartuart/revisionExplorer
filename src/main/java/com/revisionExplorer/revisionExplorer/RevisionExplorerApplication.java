7c6ae8d85158b943093569f9103379126d464328
bartuart <arturs.bartusevics@tieto.com>
2019-02-25 22:13:56 +0200

    Added new project

---
+++ src/main/java/com/revisionExplorer/revisionExplorer/RevisionExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.revisionExplorer.revisionExplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisionExplorerApplication.class, args);
+	}
+
+}

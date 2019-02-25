35e0d6fd3704d5d02d1aa1b5432f88414d29e13b
bartuart <arturs.bartusevics@tieto.com>
2019-02-25 22:24:39 +0200

    The first working version

--- src/main/java/com/revisionExplorer/revisionExplorer/RevisionExplorerApplication.java
+++ src/main/java/com/revisionExplorer/revisionExplorer/RevisionExplorerApplication.java
@@ -2,12 +2,20 @@ package com.revisionExplorer.revisionExplorer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class RevisionExplorerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RevisionExplorerApplication.class, args);
+	}
+
+	@RequestMapping("/success")
+	public String showHelloWorld(){
+		return "hello-world";
 	}
 
 }
